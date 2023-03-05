b561236700224968b14154949e0a6808e6554890
bloppers <wencheng1298@gmail.com>
2023-03-05 14:06:21 +0800

    chore: final fixes

--- experiments/experiments.go
+++ experiments/experiments.go
@@ -19,7 +19,7 @@ func Experiments(blockSize uint8) {
 	disk := storage.CreateDisk(100, blockSize)
 	disk.LoadData("./data.tsv")
 
-	fmt.Println("=== Experiment 1 ===")
+	fmt.Printf("---------- Experiment 1 ----------")
 	fmt.Printf("Number of records on disk: %d\n", disk.NumRecords)
 	fmt.Printf("Size of record: %d\n", storage.RecordLength)
 	fmt.Printf("Number of records in block: %d\n", disk.BlockArray[0].NumRecord) //disk.BlockSize/storage.RecordLength
@@ -36,8 +36,6 @@ func Experiments(blockSize uint8) {
 	tree := index.NewTree(int(treeOrder))
 
 	fmt.Println("Constructing B+ Tree...")
-	// totalBlocks := len(disk.BlockArray)
-	// Inserting records
 
 	allRecords := disk.RetrieveAll()
 	for i, record := range allRecords {
@@ -50,7 +48,7 @@ func Experiments(blockSize uint8) {
 	}
 	fmt.Println("B+ Tree Constructed")
 
-	fmt.Println("\n=== Experiment 2 ===\n")
+	fmt.Printf("\n---------- Experiment 2 ----------\n")
 	fmt.Printf("Order/Branching Factor n: %d\n", index.MAX_NUM_KEYS)
 	fmt.Printf("Number of nodes: %v\n", tree.GetTotalNodes())
 	fmt.Printf("Tree height: %v\n", tree.GetHeight())
@@ -64,24 +62,22 @@ func Experiments(blockSize uint8) {
 		• the running time of the retrieval process (please specify the method you use for measuring the running time of a piece of code)
 		• the number of data blocks that would be accessed by a brute-force linear scan method (i.e., it scans the data blocks one by one) and its running time (for comparison)
 	*/
-	fmt.Println("\n=== Experiment 3 ===\n")
+	fmt.Printf("\n=== Experiment 3 ===\n")
 	exp3Query := index.SearchConfig{
 		Type:   index.ValueQuery,
 		Values: []uint32{500},
 	}
 	exp3StartTime := time.Now()
 	recordLocationArray := tree.Search(exp3Query, true)
-	fmt.Printf("No. of RecordLocations: %v\n", len(recordLocationArray))
 	var avgOfAvgRating float32
 	for _, recordLoc := range recordLocationArray {
 		res := disk.RetrieveRecord(*recordLoc)
 		avgOfAvgRating += float32(res.AverageRating)
 	}
+	noOfBlocks := CountBlockAccessed(recordLocationArray)
 	avgOfAvgRating /= float32(len(recordLocationArray))
 	exp3EndTime := time.Now()
-	fmt.Printf("Avg of AvgRating: %v \n", avgOfAvgRating)
 	exp3TimeTaken := exp3EndTime.Sub(exp3StartTime)
-	fmt.Printf("Exp 3 Time taken: %v\n", exp3TimeTaken)
 
 	// bruteforce search
 	var search = [2]uint32{500, 500}
@@ -89,7 +85,10 @@ func Experiments(blockSize uint8) {
 	var _, bruteBlocksAccessed = disk.BruteForceSearch(search)
 	t := time.Now()
 	elapsedBruteForce := t.Sub(start)
+	fmt.Printf("No of blocks processed: %v \n", noOfBlocks)
+	fmt.Printf("Avg of AvgRating: %v \n", avgOfAvgRating)
 	fmt.Printf("Number of data blocks accessed by brute-force linear scan: %d\n", bruteBlocksAccessed)
+	fmt.Printf("Exp 3 Time taken: %v\n", exp3TimeTaken)
 	fmt.Printf("Running time of brute-force linear scan: %v\n", elapsedBruteForce)
 
 	/*
@@ -100,7 +99,7 @@ func Experiments(blockSize uint8) {
 		• the running time of the retrieval process;
 		• the number of data blocks that would be accessed by a brute-force linear scan method (i.e., it scans the data blocks one by one) and its running time (for comparison)
 	*/
-	fmt.Println("\n=== Experiment 4 ===\n")
+	fmt.Printf("\n---------- Experiment 4 ----------\n")
 	type SearchConfig struct {
 		Type   string // RangeQuery or Value query
 		Values []uint32
@@ -112,18 +111,16 @@ func Experiments(blockSize uint8) {
 	}
 	exp4StartTime := time.Now()
 	recordLocationArray = tree.Search(exp4Query, true)
-	fmt.Printf("No. of RecordLocations: %v\n", len(recordLocationArray))
 	var avgOfAvgRatingExp4 float32
 	for _, recordLoc := range recordLocationArray {
 		// fmt.Printf("RecordLocation: %v", *recordLoc)
 		res := disk.RetrieveRecord(*recordLoc)
 		avgOfAvgRatingExp4 += float32(res.AverageRating)
 	}
+	noOfBlocksExp4 := CountBlockAccessed(recordLocationArray)
 	avgOfAvgRatingExp4 /= float32(len(recordLocationArray))
 	exp4EndTime := time.Now()
-	fmt.Printf("Avg of AvgRating: %v \n", avgOfAvgRatingExp4)
 	exp4TimeTaken := exp4EndTime.Sub(exp4StartTime)
-	fmt.Printf("Exp 4 Time taken: %v\n", exp4TimeTaken)
 
 	// // bruteforce search
 	var search2 = [2]uint32{30000, 40000}
@@ -131,6 +128,9 @@ func Experiments(blockSize uint8) {
 	var _, bruteBlocksAccessed2 = disk.BruteForceSearch(search2)
 	t2 := time.Now()
 	elapsedBruteForce2 := t2.Sub(start2)
+	fmt.Printf("No of blocks processed: %v \n", noOfBlocksExp4)
+	fmt.Printf("Exp 4 Time taken: %v\n", exp4TimeTaken)
+	fmt.Printf("Avg of AvgRating: %v \n", avgOfAvgRatingExp4)
 	fmt.Printf("Number of data blocks accessed by brute-force linear scan: %d\n", bruteBlocksAccessed2)
 	fmt.Printf("Running time of brute-force linear scan: %v\n", elapsedBruteForce2)
 
@@ -165,11 +165,20 @@ func Experiments(blockSize uint8) {
 	var _, bruteBlocksAccessed5 = disk.BruteForceSearch(search5)
 	t5 := time.Now()
 	elapsedBruteForce5 := t5.Sub(start5)
-	fmt.Println("\n=== Experiment 5 ===")
+	fmt.Println("\n---------- Experiment 5 ----------")
 	fmt.Printf("\nTotal nodes of the tree: %d", totalNodes)
 	fmt.Printf("\nNo. of levels of the tree: %d", height)
 	fmt.Println("\nRoot node keys: ", tree.Root.Key)
 	fmt.Printf("\nExp 5 Time taken (B+ Tree): %v", exp5TimeTaken)
 	fmt.Printf("\nNumber for data blocks the process access (BruteFroce): %d\n", bruteBlocksAccessed5)
 	fmt.Printf("\nRunning time of brute-force linear scan: %v\n", elapsedBruteForce5)
+}
+
+func CountBlockAccessed(recordLoc []*storage.RecordLocation) int {
+	seen := make(map[uint32]int)
+	for _, record := range recordLoc {
+		seen[record.BlockIndex] += 1
+	}
+	fmt.Printf("Blocks used count map: %v \n", seen)
+	return len(seen)
 }
--- main.go
+++ main.go
@@ -6,24 +6,3 @@ func main() {
 	// run experiments.go
 	experiments.Experiments(200)
 }
-
-/*
-	// initialise disk
-	fmt.Println("Initialising disk and record")
-	var diskObject storage.Disk
-	var recordLocation storage.RecordLocation
-	diskObject = storage.CreateDisk(100, 100)
-	diskObject.CreateRecord("hello", 1.0, 1)
-	// test retrieve all
-	fmt.Println("----Test RetrieveAll()----")
-	fmt.Println(diskObject.RetrieveAll())
-	// test retrieve recordlocation
-	fmt.Println("----Test RetrieveRecord----")
-	fmt.Println(diskObject.RetrieveRecord(recordLocation))
-	// // test delete record
-	fmt.Println("----Test DeleteRecord----")
-	fmt.Println(diskObject.RetrieveAll())
-	diskObject.DeleteRecord(recordLocation)
-	fmt.Println(diskObject.RetrieveAll())
-}
-*/
